26b3cfdde7e9a1f9881dde4ac6f2771b505b8616
vinicius-07gomes <vinicius_07gomes@hotmail.com>
2024-11-15 19:05:56 -0300

    Desenvolvimento layout

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace MauiAppHotel
+﻿
+namespace MauiAppHotel
 {
     public partial class App : Application
     {
@@ -6,7 +7,14 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
+        }
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+           var window = base.CreateWindow(activationState);
+            window.Width = 400;
+            window.Height = 600;
+            return window;
         }
     }
 }
